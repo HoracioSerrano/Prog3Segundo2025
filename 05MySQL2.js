3e4e2796879c3cab8627514fdb42f23edd5a0ba0
Serrano Horacio <serranohoraciohector@gmail.com>
2025-10-22 19:38:55 -0300

    dd

--- 05MySQL2.js
+++ 05MySQL2.js
@@ -12,16 +12,19 @@ async function inicializarBaseDeDatos() {
     try{
         let qry = 'drop table clientes';
         let resultado = await db.execute(qry);
+        console.log(resultado);
     }catch{}
 
     try{
         let qry = 'CREATE TABLE clientes (id INT AUTO_INCREMENT PRIMARY KEY, nombre VARCHAR(100) NOT NULL, telefono VARCHAR(20), fecha_alta DATE DEFAULT CURRENT_DATE);';
         let resultado = await db.execute(qry);
+        console.log(resultado);
     }catch{}
 
     try{
-        let qry = 'insert into clientes(nombre, telefono, fecha_alta)values(?,?,?)';
+        let qry = `insert into clientes(nombre, telefono, fecha_alta)values(?,?,?)`;
         let resultado = await db.execute(qry,['Juan Perez', '741852963', '2025-05-20']);
+        console.log(resultado);
     }catch{}
 
     await db.end();
@@ -42,6 +45,46 @@ async function select() {
 
     console.log(res);
 
+    await db.end();
 }
 
-select();
+//select();
+
+
+async function update() {
+    const db = await mysql.createConnection({
+        host:'localhost',
+        user:'root',
+        password:'',
+        database:'dbprueba'
+    });
+
+    let res = await db.execute('update clientes set nombre = ? where id= ? ', ['PrezJuan',1]);
+
+    console.log(res);
+
+    await db.end();
+}
+
+//update();
+
+
+
+//controller
+app.post('/insertarUsuario',async (req,res,next)=>{
+    let body = req.body;
+    await InsertarCliente(body.nombre, body.telefono);
+});
+
+//modelo
+async function InsertarCliente(nombre,telefono){
+        const db = await mysql.createConnection({
+        host:'localhost', //url para coneccion http a la base de datos
+        user:'root',//si tubiese usuario va aqui
+        password:'',//si tubiese contraseña va aqui
+        database:'dbprueba'//nombre de la base a utilizar
+    });
+    let qry = `insert into clientes(nombre, telefono, fecha_alta)values(?,?,?)`;
+    let resultado = await db.execute(qry,['Juan Perez', '741852963', new Date()]);
+    await db.end();
+}
