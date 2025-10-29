72a9c5ce066e83ae30445711ab56de76ba6fa61e
Serrano Horacio <serranohoraciohector@gmail.com>
2025-10-29 17:22:21 -0300

    Ejemplos EJS

---
+++ 10Ejs.js
@@ -0,0 +1,53 @@
+//motor de plantillas ejs
+//ejemplo renderizando con llamada a ejs
+
+const express = require('express'); //npm install express
+const app = express();
+const port = 3000;
+const path = require('path');
+const ejs = require('ejs');
+
+app.get('/', async (req, res) => {
+    const persona = {nombre:"horacio", apellido:"serrano"};
+
+    const rutaVista = path.join(__dirname, '10vistas', 'persona.ejs');
+
+    const html = await ejs.renderFile(rutaVista, {persona:persona});
+
+    res.send(html);
+});
+
+app.get('/tabla', async (req, res) => {
+    const personas = [
+            {nombre:"horacio", apellido:"serrano", sexo:1}, 
+            {nombre:"marcelo", apellido:"luque", sexo:1}, 
+            {nombre:"ramiro", apellido:"escobar", sexo:1},
+            {nombre:"maria", apellido:"cabral", sexo:2}
+        ];
+
+    const rutaVista = path.join(__dirname, '10vistas', 'tabla.ejs');
+
+    const html = await ejs.renderFile(rutaVista, {personas:personas});
+
+    res.send(html);
+});
+
+app.get('/engine', async (req, res) => {
+    const personas = [
+            {nombre:"horacio", apellido:"serrano", sexo:1}, 
+            {nombre:"marcelo", apellido:"luque", sexo:1}, 
+            {nombre:"ramiro", apellido:"escobar", sexo:1},
+            {nombre:"maria", apellido:"cabral", sexo:2}
+        ];
+
+    res.render('tabla2.ejs', {personas:personas});
+
+});
+
+
+app.set('views', path.join(__dirname, '10vistas')); // o la carpeta raíz donde están las vistas
+app.set('view engine', 'ejs'); //seteo como motor de renderizado
+
+app.listen(port, () => {
+    console.log(`Puerto ${port}`) //<-- http://localhost:3000 
+});
