c09d854d106f4ffd4071f390513bf23f0e94fc74
Serrano Horacio <serranohoraciohector@gmail.com>
2025-10-30 21:08:19 -0300

    Bcrypt

---
+++ 11bcrypt.js
@@ -0,0 +1,29 @@
+const bcrypt = require('bcrypt');
+const saltRounds = 15;
+
+async function Prueba(){
+
+    const hashedPassword= await bcrypt.hash('ClaveSecreta', saltRounds);
+    console.log('Hash a grabar en DB:', hashedPassword);
+
+    //La clave Que se ingresa en login;
+    const claveIngresadaEnLogin = 'SoyHoracio';
+    const resultado = await bcrypt.compare(claveIngresadaEnLogin, hashedPassword)
+    if(resultado){
+        console.log('Clave Correcta');
+    }else{
+        console.log('Clave Incorrecta');
+    }
+
+    //La clave Que se ingresa en login;
+    console.log('Segundo Intento:');
+    const intento2 = 'ClaveSecreta';
+    const resultado2 = await bcrypt.compare(intento2, hashedPassword)
+    if(resultado2){
+        console.log('Clave Correcta');
+    }else{
+        console.log('Clave Incorrecta');
+    }
+}
+
+Prueba();
