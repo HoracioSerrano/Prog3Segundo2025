d97e400680a00d8fbf54f18e6ac6817ddba3f6bb
Serrano Horacio <serranohoraciohector@gmail.com>
2025-10-31 20:19:32 -0300

    ss

--- 13jwsbcrypt.js
+++ 13jwsbcrypt.js
@@ -12,6 +12,29 @@ const port = 3000;
 app.use(express.json());
 
 
+/*
+enviar cookies
+app.get('/login', (req, res) => {
+  res.cookie('usuario', 'Horacio Serrano', {
+    maxAge: 1000 * 60 * 60, // 1 hora
+    httpOnly: true,          // no accesible desde JS del navegador
+    secure: false,           // true si usás HTTPS
+    sameSite: 'lax'          // evita algunos ataques CSRF
+  });
+  res.send('Cookie segura seteada!');
+});
+
+leer cookies
+const cookieParser = require('cookie-parser');
+const app = express();
+app.use(cookieParser());
+app.get('/ver', (req, res) => {
+  res.send(req.cookies); // muestra todas las cookies
+});
+
+*/
+
+
 
 
 app.listen(port, () => {
